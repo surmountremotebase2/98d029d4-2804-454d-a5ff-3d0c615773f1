2bc442da0072e1a44d1fd0194913307e5b42f401
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-10 17:58:22 -0700

    User Moby Investments saved changes to main.py at 11/11/2024, 00:58:21 UTC time.

--- 5ad9736e-a126-48df-aed7-919d9bf8b5e1/main.py
+++ 5ad9736e-a126-48df-aed7-919d9bf8b5e1/main.py
@@ -1,1 +1,43 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+from datetime import datetime
+
+class TradingStrategy(Strategy):
+
+   def __init__(self):
+      self.tickers = ["EMPH", "MCHP", "GOOGL", "KLAC"]
+      self.weights = [0.39, 0.47, 0.07, 0.07]
+      self.equal_weighting = False
+      self.counter = 0
+
+   @property
+   def interval(self):
+      return "1day"
+
+   @property
+   def assets(self):
+      return self.tickers
+
+   def run(self, data):
+      if len(data['ohlcv']) < 2:
+         self.counter += 1
+         if self.counter >= 30:
+            self.counter = 0
+            if self.equal_weighting: 
+               allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+            else:
+               allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
+            return TargetAllocation(allocation_dict)
+         else:
+            return None
+
+      today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
+      yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
+      
+      if today.day == 12 or (today.day > 12 and yesterday.day < 12):
+         if self.equal_weighting: 
+            allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+         else:
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
+         return TargetAllocation(allocation_dict)
+      return None
