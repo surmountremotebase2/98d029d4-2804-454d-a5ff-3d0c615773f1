88e78037370584e90f6ca1b1bfaa6cb9071615c8
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-10 18:00:58 -0700

    User Moby Investments saved changes to main.py at 11/11/2024, 01:00:57 UTC time.

--- 5ad9736e-a126-48df-aed7-919d9bf8b5e1/main.py
+++ 5ad9736e-a126-48df-aed7-919d9bf8b5e1/main.py
@@ -19,18 +19,6 @@ class TradingStrategy(Strategy):
       return self.tickers
 
    def run(self, data):
-      if len(data['ohlcv']) < 2:
-         self.counter += 1
-         if self.counter >= 30:
-            self.counter = 0
-            if self.equal_weighting: 
-               allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
-            else:
-               allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
-            return TargetAllocation(allocation_dict)
-         else:
-            return None
-
       today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
       yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
       
